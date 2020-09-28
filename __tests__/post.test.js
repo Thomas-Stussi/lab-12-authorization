0752ac9e0d0747df7d6714a6bf880a8ac8989882
Thomas Stussi <tstussi06@gmail.com>
2020-09-28 16:02:09 -0700

    node ci fix

--- __tests__/post.test.js
+++ __tests__/post.test.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
-const UserService = require('../lib/services/user-service');
 
 describe('post routes', () => {
   beforeEach(() => {

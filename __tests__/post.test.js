74797cb4d75644e67319f25dbc659e14f6efb342
Thomas Stussi <tstussi06@gmail.com>
2020-09-28 17:01:03 -0700

    post model

--- __tests__/post.test.js
+++ __tests__/post.test.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/user-service');
 
 describe('post routes', () => {
   beforeEach(() => {
@@ -9,7 +10,10 @@ describe('post routes', () => {
   });
 
   it('makes a new post via POST', async () => {
-    const response = await request(app)
-      .post('/api/v1/auth/signup')
+    const user = await UserService.create({
+      email: 'test@test.com',
+      profile: 'test',
+      password: 'password'
+    });
   })
 });
